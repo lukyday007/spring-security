c055ee8f8382f3a0555dee6957d5e63eb9888d06
dayul <rainbow9517@naver.com>
2025-02-19 17:37:55 +0900

    [study] spring jwt

---
+++ jwt/src/main/java/com/spring_security/jwt/controller/JoinController.java
@@ -0,0 +1,26 @@
+package com.spring_security.jwt.controller;
+
+import com.spring_security.jwt.dto.JoinDTO;
+import com.spring_security.jwt.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class JoinController {
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        System.out.println(joinDTO.getUsername());
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+}
---
+++ jwt/src/main/java/com/spring_security/jwt/dto/JoinDTO.java
@@ -0,0 +1,11 @@
+package com.spring_security.jwt.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class JoinDTO {
+
+    private String username;
+    private String password;
+}
